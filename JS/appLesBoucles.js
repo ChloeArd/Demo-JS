3c4979284be85f9f9c4d9b828bcf52bd9cf2b936
ChloeArd <chloe.ardoise@gmail.com>
2020-11-30 13:09:47 +0100

    Demo JavaScript

---
+++ JS/appLesBoucles.js
@@ -0,0 +1,15 @@
+/**
+ *La boucle while
+ */
+let elements = document.getElementsByTagName('p'); //On récupère tous les éléments de type paragraphe. (TOUS) et il y en a plusieurs.
+console.log(elements); //On a bien plusieurs éléments.
+console.log("Test de la taille de la collection: " + elements.length);
+
+/**
+ *La boucle do ... while
+ */
+
+
+/**
+ *La boucle for
+ */
