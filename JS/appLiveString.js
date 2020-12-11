68a6c4bf918318810b3cd11b33c69b8b34207c02
ChloeArd <chloe.ardoise@gmail.com>
2020-12-11 14:18:29 +0100

    Demo JavaScript

---
+++ JS/appLiveString.js
@@ -0,0 +1,5 @@
+let resultatP = document.getElementById('resultat');
+let testStringP = document.getElementById("testString");
+
+resultatP.innerHTML = "Mon paragraphe contient " + testStringP.innerHTML.length + " carcatères";
+
